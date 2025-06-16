3d26a5e6cda9210e60977c6167316b4f7d52e509
Lakyzz <ayax.bugueno.a@gmail.com>
2025-06-15 22:54:50 -0400

    feat(home): Add product grid an carousel - implemented Glide.js for carousel - Update Bootstrap card style - Add templates dirs in settings

--- catalogoHome/views.py
+++ catalogoHome/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Product
+from itertools import islice
+from django.views.generic import ListView
 
 
 def catalogo_home(request):
     products = Product.objects.all()
 
     return render(request,'catalogoHome/index.html',{'products':products})
+
